a32f227a634885a5043bc610b090a7b11f6ba1c9
romaninrome <189551357+romaninrome@users.noreply.github.com>
2025-10-04 14:08:06 +0200

    Removed Node dependencies (well, most of them)

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,3 @@
-// import dotenv from 'dotenv';
-
-// dotenv.config();
-
-// export const config = {
-//     telegramToken: process.env.TELEGRAM_BOT_TOKEN || '',
-//     apiUrl: process.env.API_URL || '',
-//     authToken: process.env.AUTH_TOKEN || ''
-// };
-import dotenv from 'dotenv';
-
-dotenv.config();
-
 export const config = {
     telegramToken: process.env.TELEGRAM_BOT_TOKEN || '',
     apiUrl: process.env.API_URL || '',
